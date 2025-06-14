c347551da1f077cc9a6bc833b0f335d54a5f73bf
maulanasidiq <lbisa4896@gmail.com>
2025-06-14 11:32:32 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/praktikum6m/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.praktikum6m
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Muhammad Maulana Sidiq',
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: Center(
+          child: Text('Halo Selamat Datang Di Flutter'),
+        ),
+      ),
+    );
+  }
+}
